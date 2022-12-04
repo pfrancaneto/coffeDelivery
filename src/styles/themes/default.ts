950000b5bd0affa433b116f0fcade9095607a5b3
Pedro França <pfranca.neto@gmail.com>
2022-12-04 18:43:10 -0300

    ajustes projeto

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,23 @@
+export const defaultTheme = {
+  //Produto
+  yellow_dark: '#C47F17',
+  yellow: '#DBAC2C',
+  yellow_light: '#F1E9C9',
+  purple_dark: '#4B2995',
+  purple: '#8047F8',
+  purple_light: '#EBE5F9',
+
+
+  //Base
+  white: '#ffffff',
+  background: '#fafafa',
+
+  base_card: '#f3f2f2',
+  base_input: '#EDEDED',
+  base_button: '#E6E5E5',
+  base_hover: '#D7D5D5',
+  base_label: '#8D8686',
+  base_text: '#574F4D',
+  base_subtitle: '#403937',
+  base_title: '#272221',
+}
