5b79b00d843aeeca62c27aa78825f67367ec40f7
Pedro França <pfranca.neto@gmail.com>
2022-12-04 21:56:27 -0300

    add BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
+import { GlobalStyle } from './styles/global';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Home</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
 
