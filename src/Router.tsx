a3d26823aee8a195bad383c4ce79516ba7f95abd
Pedro França <pfranca.neto@gmail.com>
2022-12-04 21:54:26 -0300

    criando arquivos de rotas

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import { DefaultLayout } from './Layouts/DefaultLayout';
+import { Checkout } from './pages/Checkout';
+import { Home } from './pages/Home';
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<DefaultLayout />}>
+        <Route path='/' element={<Home />}/>
+        <Route path='/checkout' element={<Checkout />}/>
+      </Route>
+    </Routes>
+  );
+}
