d00b2368e16da4a151197b76e724402c97968804
Pedro França <pfranca.neto@gmail.com>
2022-12-10 22:24:28 -0300

    refatorando DefaultLayout

--- src/Layouts/DefaultLayout/styles.ts
+++ src/Layouts/DefaultLayout/styles.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const DefaultLayoutContainer = styled.div`
-  max-width: 74rem;
-  margin: 0 auto;
-
-  background-color: ${(props) => props.theme['background']};
-  display: flex;
-  flex-direction: column;
+  .container {
+    width: 100%;
+    max-width: 70rem;
+    margin-right: auto;
+    margin-left: auto;
+  }
 `;
