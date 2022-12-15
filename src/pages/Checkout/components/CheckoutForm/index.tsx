39daf6e60b1cd9e6cb87c7191ead3a6a42160c0e
Pedro França <pfranca.neto@gmail.com>
2022-12-15 10:29:28 -0300

    add component CheckouForm

---
+++ src/pages/Checkout/components/CheckoutForm/index.tsx
@@ -0,0 +1,29 @@
+import { CurrencyDollar, MapPinLine } from 'phosphor-react';
+import { TituloSection } from '../TituloSection';
+import { EnderecoForm } from './EnderecoForm';
+import { FormaPagamento } from './FormaPagamento';
+import { CheckoutFormContainer, SectionFormContainer, Title } from './styles';
+
+export const CheckoutForm = () => {
+  return (
+    <CheckoutFormContainer>
+      <Title>Complete o seu Pedido</Title>
+      <SectionFormContainer>
+        <TituloSection
+          titulo="Endereço de Entrega"
+          subTitulo="Informe o endereço onde deseja receber seu pedido"
+          icone={<MapPinLine color='#C47F17' size={22} />}
+        />
+        <EnderecoForm />
+      </SectionFormContainer>
+      <SectionFormContainer>
+        <TituloSection 
+          titulo='Pagamento'
+          subTitulo='O pagamento é feito na entrega. Escolha a forma que deseja pagar'
+          icone={<CurrencyDollar color='#8047F8' size={22} />} 
+        />
+        <FormaPagamento />
+      </SectionFormContainer>
+    </CheckoutFormContainer>
+  );
+};
